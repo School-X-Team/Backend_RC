436ce9fc6ce4d1c96e90baa089d848fb41f2c0a3
Neoenchik <102302057+Neoenchik@users.noreply.github.com>
2025-03-18 03:58:50 +0300

    add new migrate

---
+++ Migrations/20250318005751_EventsMigrate.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend_RC.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventsMigrate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "StartTime",
+                table: "Events",
+                newName: "Date");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Events",
+                newName: "Title");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AdditionalInfo",
+                table: "Events",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AgeRating",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Subtitle",
+                table: "Events",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Time",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdditionalInfo",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "AgeRating",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Subtitle",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Events",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Events",
+                newName: "StartTime");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndTime",
+                table: "Events",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Events",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Events",
+                type: "numeric(10,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
