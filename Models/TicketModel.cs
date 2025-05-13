51575f4954b8c84cc7ef4d3068925ee616acbf64
Neoenchik <102302057+Neoenchik@users.noreply.github.com>
2025-05-14 02:35:20 +0300

    delete ticketmodel

--- Models/TicketModel.cs
+++ Models/TicketModel.cs
@@ -1,23 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿//using System.ComponentModel.DataAnnotations;
 
-namespace Backend_RC.Models;
-/// <summary>
-/// Модель для билетов на мероприятия
-/// </summary>
-public class TicketModel
-{
-    public string Id { get; set; }
+//namespace Backend_RC.Models;
+///// <summary>
+///// Модель для билетов на мероприятия
+///// </summary>
+//public class TicketModel
+//{
+//    public string Id { get; set; }
 
-    [Required]
-    public string UserId { get; set; }
+//    [Required]
+//    public string UserId { get; set; }
 
-    [Required]
-    public int EventId { get; set; }
+//    [Required]
+//    public int EventId { get; set; }
 
-    public DateTime PurchasedAt { get; set; } = DateTime.UtcNow;
+//    public DateTime PurchasedAt { get; set; } = DateTime.UtcNow;
 
-    //Навигационные свойства
-    public virtual EventModel Event { get; set; }
+//    //Навигационные свойства
+//    public virtual EventModel Event { get; set; }
 
-    public virtual User User { get; set; }
-}
+//    public virtual User User { get; set; }
+//}
