2d2b9b8ecf09b6480249e3e73cfccc0656b57f8e
Neoenchik <102302057+Neoenchik@users.noreply.github.com>
2025-05-14 02:43:30 +0300

    add new migrations

---
+++ Migrations/20250513234319_AddedNewFieldsToUser.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Backend_RC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNewFieldsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_AspNetUsers_UserId",
+                table: "Tickets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Events_EventId",
+                table: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_EventId",
+                table: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_UserId",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "EventId",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "PurchasedAt",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "AdditionalInfo",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Subtitle",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Tickets",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "Time",
+                table: "Events",
+                newName: "Type");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Events",
+                newName: "TimeStart");
+
+            migrationBuilder.RenameColumn(
+                name: "Genre",
+                table: "Events",
+                newName: "TimeEnd");
+
+            migrationBuilder.RenameColumn(
+                name: "AgeRating",
+                table: "Events",
+                newName: "TakeTime");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Tickets",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Date",
+                table: "Tickets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Tickets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Time",
+                table: "Tickets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DateEnd",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DateStart",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double[]>(
+                name: "EndCoordinates",
+                table: "Events",
+                type: "double precision[]",
+                nullable: false,
+                defaultValue: new double[0]);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Price",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Reward",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double[]>(
+                name: "StartCoordinates",
+                table: "Events",
+                type: "double precision[]",
+                nullable: false,
+                defaultValue: new double[0]);
+
+            migrationBuilder.CreateTable(
+                name: "Routes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Time = table.Column<string>(type: "text", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    StartCoordinates = table.Column<double[]>(type: "double precision[]", nullable: false),
+                    EndCoordinates = table.Column<double[]>(type: "double precision[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Routes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Icon = table.Column<string>(type: "text", nullable: false),
+                    TaskPointStart = table.Column<string>(type: "text", nullable: false),
+                    TaskPointEnd = table.Column<string>(type: "text", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    TaskDescription = table.Column<string>(type: "text", nullable: false),
+                    Reward = table.Column<string>(type: "text", nullable: false),
+                    TaskCityType = table.Column<string>(type: "text", nullable: false),
+                    TaskCityPlaceInfo = table.Column<string>(type: "text", nullable: false),
+                    StartCoordinates = table.Column<double[]>(type: "double precision[]", nullable: false),
+                    EndCoordinates = table.Column<double[]>(type: "double precision[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Routes");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "DateEnd",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "DateStart",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "EndCoordinates",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Reward",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "StartCoordinates",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Tickets",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Events",
+                newName: "Time");
+
+            migrationBuilder.RenameColumn(
+                name: "TimeStart",
+                table: "Events",
+                newName: "ImageUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "TimeEnd",
+                table: "Events",
+                newName: "Genre");
+
+            migrationBuilder.RenameColumn(
+                name: "TakeTime",
+                table: "Events",
+                newName: "AgeRating");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Tickets",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EventId",
+                table: "Tickets",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PurchasedAt",
+                table: "Tickets",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "AdditionalInfo",
+                table: "Events",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Events",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Subtitle",
+                table: "Events",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_EventId",
+                table: "Tickets",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_UserId",
+                table: "Tickets",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_AspNetUsers_UserId",
+                table: "Tickets",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Events_EventId",
+                table: "Tickets",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
